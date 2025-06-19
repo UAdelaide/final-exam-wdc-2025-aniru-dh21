fce4981b4b31584279c59fa1285695b5f4c5ff78
Ramachandra Anirudh Vemulapalli <vemulapallianirudh18@gmail.com>
2025-06-19 23:46:43 +0000

    [automatic commit] 2025-06-19 23:46:43 UTC 101.115.225.174

--- starthere/app.js
+++ starthere/app.js
@@ -30,7 +30,7 @@ let db;
     db = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
-      password: '',
+      password: 'anirudh@2003',
       database: 'testdb'
     });
 
