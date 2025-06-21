b97461e600e83bc3ad740ff7658a51413fea98b4
Ramachandra Anirudh Vemulapalli <vemulapallianirudh18@gmail.com>
2025-06-21 10:09:08 +0930

    Commit

--- part2/models/db.js
+++ part2/models/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'your_database_password',
   database: 'DogWalkService',
   waitForConnections: true,
   connectionLimit: 10,
